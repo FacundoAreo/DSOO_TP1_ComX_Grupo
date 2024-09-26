307e9d4c9becf74e009b779e0e3b9ab5843fee75
Facundo Areo Sanchez <fas4005@hotmail.com>
2024-09-25 21:16:12 -0300

    /*  * User: Estudiante Equipo 5  * Sanchez, Facundo Areo , facundo.areo@dgmail.com  * Briant Alcides Gauna, gauna.br@gmail.com  * Esteban Tomás González. ebanban77@gmail.com  * Agostina Milena poliagostinamilena@gmail.com  * link https://github.com/FacundoAreo/DSOO_TP1_ComX_Grupo  * Date: 24/9/2024  * Time: 20:08  *  * To change this template use Tools | Options | Coding | Edit Standard Headers.  */

--- DSOO_TP1_ComX_GrupoX/Actividad.cs
+++ DSOO_TP1_ComX_GrupoX/Actividad.cs
@@ -1,8 +1,12 @@
 ﻿/*
- * Created by SharpDevelop.
- * User: Estudiante
+ * User: Estudiante Equipo 5
+ * Sanchez, Facundo Areo , facundo.areo@dgmail.com
+ * Briant Alcides Gauna, gauna.br@gmail.com
+ * Esteban Tomás González. ebanban77@gmail.com
+ * Agostina Milena poliagostinamilena@gmail.com
+ * link https://github.com/FacundoAreo/DSOO_TP1_ComX_Grupo
  * Date: 24/9/2024
- * Time: 20:10
+ * Time: 20:08
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
--- DSOO_TP1_ComX_GrupoX/Program.cs
+++ DSOO_TP1_ComX_GrupoX/Program.cs
@@ -1,6 +1,10 @@
 ﻿/*
- * Created by SharpDevelop.
- * User: Estudiante
+ * User: Estudiante Equipo 5
+ * Sanchez, Facundo Areo , facundo.areo@dgmail.com
+ * Briant Alcides Gauna, gauna.br@gmail.com
+ * Esteban Tomás González. ebanban77@gmail.com
+ * Agostina Milena poliagostinamilena@gmail.com
+ * link https://github.com/FacundoAreo/DSOO_TP1_ComX_Grupo
  * Date: 24/9/2024
  * Time: 20:08
  * 
