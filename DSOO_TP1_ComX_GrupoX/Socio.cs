35239310822407345417b8ddb462f0721b55671b
Facundo Areo Sanchez <fas4005@hotmail.com>
2024-09-25 21:29:06 -0300

    /*  * User: Estudiante Equipo 5  * Sanchez, Facundo Areo , facundo.areo@dgmail.com  * Briant Alcides Gauna, gauna.br@gmail.com  * Esteban Tomás González. ebanban77@gmail.com  * Poli Agostina Milena poliagostinamilena@gmail.com  * link https://github.com/FacundoAreo/DSOO_TP1_ComX_Grupo  * Date: 24/9/2024  * Time: 20:08  *  * To change this template use Tools | Options | Coding | Edit Standard Headers.  */

--- DSOO_TP1_ComX_GrupoX/Socio.cs
+++ DSOO_TP1_ComX_GrupoX/Socio.cs
@@ -3,7 +3,7 @@
  * Sanchez, Facundo Areo , facundo.areo@dgmail.com
  * Briant Alcides Gauna, gauna.br@gmail.com
  * Esteban Tomás González. ebanban77@gmail.com
- * Agostina Milena poliagostinamilena@gmail.com
+ * Poli Agostina Milena poliagostinamilena@gmail.com
  * link https://github.com/FacundoAreo/DSOO_TP1_ComX_Grupo
  * Date: 24/9/2024
  * Time: 20:08
